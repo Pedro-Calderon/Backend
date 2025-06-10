d65cf21cedbebe74cbf6124f1b488043404efd24
Pedro-Calderon <s18120164@alumnos.itsur.edu.mx>
2025-06-09 19:59:40 -0600

    BackEnd initialized

---
+++ src/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Servidor backend funcionando 🚀");
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
