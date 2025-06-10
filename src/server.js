462849758a242f968394b89293ca80396eefa41e
Pedro-Calderon <s18120164@alumnos.itsur.edu.mx>
2025-06-09 20:20:52 -0600

    added conection to database

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Servidor backend funcionando 🚀");
