44cdf4bee77df5a87f67edef2476491b23e18533
Pedro-Calderon <s18120164@alumnos.itsur.edu.mx>
2025-06-11 01:46:34 -0600

    Ajustes

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
-
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+}
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);  
+    await mongoose.connect(process.env.MONGODB_URI,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('✅ MongoDB conectado');
   } catch (error) {
     console.error('❌ Error conectando a MongoDB:', error);
